67c1aa83f2d78eb040351d88ee536ce0e9561ed2
Damien Milon <37899667+dmilon@users.noreply.github.com>
2019-11-25 14:24:03 +0100

    Removed devise

--- db/schema.rb
+++ db/schema.rb
@@ -31,17 +31,5 @@ ActiveRecord::Schema.define(version: 2019_11_25_113109) do
     t.index ["garden_id"], name: "index_plants_on_garden_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "plants", "gardens"
 end
