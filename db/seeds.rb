433fc97a10ac4c1a39fabfa319244f0714c8b788
Damien Milon <37899667+dmilon@users.noreply.github.com>
2019-11-27 12:41:39 +0100

    Styling card grid...

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,8 @@
 Garden.destroy_all if Rails.env.development?
 
-Garden.create!(
-  name: "My Little Garden",
-  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
-)
-
-Garden.create!(
-  name: "My Other Garden",
-  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
-)
+6.times do
+  Garden.create!(
+    name: "#{Faker::Name.unique.name}'s garden",
+    banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
+  )
+end
